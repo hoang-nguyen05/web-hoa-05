32159629e4f28a706633db60e7856cd75e24ea59
hoang-nguyen05 <dtc235200306@ictu.edu.vn>
2026-05-05 18:40:48 +0700

    Ghi chú nội dung

--- shop/views.py
+++ shop/views.py
@@ -276,5 +276,5 @@ def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
     return render(request, 'shop/product/detail.html', {'product': product})
 
- def abouttt(request):
+ def abouttttttttttttt(request):
     return render(request, 'shop/about.html') 
