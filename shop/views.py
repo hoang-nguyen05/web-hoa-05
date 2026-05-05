6036325de8e8e66c76ff3df817629dc6f5f29d93
hoang-nguyen05 <dtc235200306@ictu.edu.vn>
2026-05-05 23:55:28 +0700

    tải lên

--- shop/views.py
+++ shop/views.py
@@ -275,6 +275,5 @@ def product_list(request, category_slug=None):
 def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
     return render(request, 'shop/product/detail.html', {'product': product})
-
- def aboutsadtt(request):
-    return render(request, 'shop/about.html') 
+def abouuuuuuuut(request):
+    return render(request, 'shop/about.html')
