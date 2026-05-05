93df8d5fc6e11f749897a0842793aa85391dcab4
hoang-nguyen05 <dtc235200306@ictu.edu.vn>
2026-05-05 18:35:34 +0700

    Ghi chú

--- shop/views.py
+++ shop/views.py
@@ -275,3 +275,6 @@ def product_list(request, category_slug=None):
 def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
     return render(request, 'shop/product/detail.html', {'product': product})
+
+ def about(request):
+    return render(request, 'shop/about.html') 
